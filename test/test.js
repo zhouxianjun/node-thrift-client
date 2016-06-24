597070927b39ad0ed3d051b627a237ba2b2dd752
zhouxianjun <gary@cn-face.com>
2016-06-24 20:07:03 +0800

    修改index暴露

--- test/test.js
+++ test/test.js
@@ -28,7 +28,7 @@
 const ThriftClient = require('../lib/index');
 let demo = require('./thrift/Demo');
 
-var client = ThriftClient.zookeeper.createClient('www.cn-face.com:2181');
+var client = ThriftClient.zookeeper.createClient('127.0.0.1:2181');
 client.connect();
 client.on('connected', function() {
     let providerFactory = new ThriftClient.provider.ZookeeperThriftServerProviderFactory(
@@ -44,9 +44,13 @@ client.on('connected', function() {
         thriftClient.on('fileSystemInit', () => {
             const DemoService = require('../test/service/DemoService');
             let demoService = DemoService.instance();
+            let start = new Date();
             for (let i = 0; i < 100; i++) {
                 demoService.say('Gary').then(result => {
                     console.log(`result<${i}>:${result}`);
+                    if (i == 99) {
+                        console.log(`running time: ${new Date().getTime() - start.getTime()}`);
+                    }
                 }, err => {
                     console.log('error ~');
                     console.error(err.stack);
